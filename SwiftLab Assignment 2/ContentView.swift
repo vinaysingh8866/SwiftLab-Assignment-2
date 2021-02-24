8c04b3806d9ff398dc612e298d5bb3b5c2f8c5a0
Vinay Singh <vinaysingh8866@gmail.com>
2021-02-24 15:26:19 +0530

    Initial Commit

---
+++ SwiftLab Assignment 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLab Assignment 2
+//
+//  Created by Vinay Singh on 24/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
